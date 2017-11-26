80c221eefeedb5d0060501ce5ec357f152876097
Sean Aguirre <sean.aguirre@soleranetwork.com>
2017-11-26 16:55:25 -0700

    added tool random number generation

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, world\n")
+	var myname string
+	var toolresponse string
+
+	nameQuestion := "What is your name?"
+	fmt.Println(nameQuestion)
+	fmt.Scanf("%s", &myname)
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	randomNum := r1.Intn(100)
+
+	if (randomNum > 51) {
+		toolresponse = "You're a tool... sorry bud"
+	} else {
+		toolresponse = "Congrats, you're not a tool. Celebrate"
+	}
+
+	fmt.Print("Hello ", myname + " " + toolresponse)
+	fmt.Println()
 }
